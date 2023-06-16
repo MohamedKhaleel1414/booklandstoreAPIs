bf58aee9b0ee3351dd8fdc811c5ee6fa79bc0994
Mohamed Khaleel <113287240+MohamedKhaleel1414@users.noreply.github.com>
2023-06-17 02:14:44 +0300

    news

--- routes/recommRoute.js
+++ routes/recommRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const recommService = require('../services/recommService')
 
-router.post("/recommendation", recommendSystem.recommendations);
+router.post("/recommendation", recommService.recommendations);
 
 module.exports = router
